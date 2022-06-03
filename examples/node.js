d6d74aa2d69bc71f020baeca5fd7e70fe6a03918
saikishore <saikishore.bits@gmail.com>
2022-06-03 06:26:32 +0530

    Add a NodeJS example

---
+++ examples/node.js
@@ -0,0 +1,109 @@
+const { execSync } = require("child_process");
+const path = require("path");
+
+const binPath = path.join(__dirname, "fidelius-1.0.0", "bin", "fidelius");
+
+const execFideliusCli = (args) => {
+	const shellCommand = process.platform !== "win32" ? "sh" : "";
+	const execOptions = { encoding: "utf-8" };
+	const fideliusCommand = `${shellCommand} ${binPath} ${args.join(" ")}`;
+
+	const result = execSync(fideliusCommand, execOptions);
+	try {
+		return JSON.parse(result.replace(/(\r\n|\n|\r)/gm, ""));
+	} catch (error) {
+		console.error(
+			`ERROR · execFideliusCli · Command: ${args.join(" ")} ·`,
+			{ result },
+			error
+		);
+	}
+};
+
+const getEcdhKeyMaterial = () => {
+	const result = execFideliusCli(["gkm"]);
+	return result;
+};
+
+const encryptData = ({
+	stringToEncrypt,
+	senderNonce,
+	requesterNonce,
+	senderPrivateKey,
+	requesterPublicKey,
+}) => {
+	const result = execFideliusCli([
+		"e",
+		`"${stringToEncrypt}"`,
+		senderNonce,
+		requesterNonce,
+		senderPrivateKey,
+		requesterPublicKey,
+	]);
+	return result;
+};
+
+const decryptData = ({
+	encryptedData,
+	requesterNonce,
+	senderNonce,
+	requesterPrivateKey,
+	senderPublicKey,
+}) => {
+	const result = execFideliusCli([
+		"d",
+		encryptedData,
+		requesterNonce,
+		senderNonce,
+		requesterPrivateKey,
+		senderPublicKey,
+	]);
+	return result;
+};
+
+const runExample = () => {
+	const stringToEncrypt = "There is no war in Ba Sing Se!";
+
+	const requesterKeyMaterial = getEcdhKeyMaterial();
+	const senderKeyMaterial = getEcdhKeyMaterial();
+
+	console.log({ requesterKeyMaterial, senderKeyMaterial });
+
+	const { encryptedData } = encryptData({
+		stringToEncrypt,
+		senderNonce: senderKeyMaterial.nonce,
+		requesterNonce: requesterKeyMaterial.nonce,
+		senderPrivateKey: senderKeyMaterial.privateKey,
+		requesterPublicKey: requesterKeyMaterial.publicKey,
+	});
+
+	const { encryptedData: encryptedDataWithX509PublicKey } = encryptData({
+		stringToEncrypt,
+		senderNonce: senderKeyMaterial.nonce,
+		requesterNonce: requesterKeyMaterial.nonce,
+		senderPrivateKey: senderKeyMaterial.privateKey,
+		requesterPublicKey: requesterKeyMaterial.x509PublicKey,
+	});
+
+	console.log({ encryptedData, encryptedDataWithX509PublicKey });
+
+	const { decryptedData } = decryptData({
+		encryptedData,
+		requesterNonce: requesterKeyMaterial.nonce,
+		senderNonce: senderKeyMaterial.nonce,
+		requesterPrivateKey: requesterKeyMaterial.privateKey,
+		senderPublicKey: senderKeyMaterial.publicKey,
+	});
+
+	const { decryptedData: decryptedDataWithX509PublicKey } = decryptData({
+		encryptedData,
+		requesterNonce: requesterKeyMaterial.nonce,
+		senderNonce: senderKeyMaterial.nonce,
+		requesterPrivateKey: requesterKeyMaterial.privateKey,
+		senderPublicKey: senderKeyMaterial.x509PublicKey,
+	});
+
+	console.log({ decryptedData, decryptedDataWithX509PublicKey });
+};
+
+runExample();
